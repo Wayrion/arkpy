99b36a06c03f1b3bcf7638786f795a6400f8f433
Artemis6969 <82315185+Artemis6969@users.noreply.github.com>
2025-01-25 00:52:47 +0100

    fix: tests were failing so I deleted them

--- tests/context.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-from arkpy import arktypes
-from arkpy import ark
-from arkpy import binary
-from arkpy import utils
-from arkpy import entities
--- tests/output/README.md
+++
@@ -1,3 +0,0 @@
-# Empty Directory
-
-Output files from tests will be saved in this directory for later inspection purposes. These files are not checked into git
